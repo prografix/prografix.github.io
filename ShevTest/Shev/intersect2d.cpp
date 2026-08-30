0b98f8591a4957a3809e13903a172d1b7478c75c
prografix <shevchenka@narod.ru>
2026-08-30 15:43:22 +0300

    calcIntersect2c

--- ShevTest/Shev/intersect2d.cpp
+++ ShevTest/Shev/intersect2d.cpp
@@ -349,6 +349,147 @@ SuiteRef<Segment2d> & intersection ( const Segment2d & seg, CArrRef<Vector2d> po
     }
     return res;
 }
+static int areaSign ( const Vector2d & a, const Vector2d & b, const Vector2d & c )
+{
+    const double d = ( b - a ) % ( c - a );
+    return d > 0 ? 1 : d < 0 ? -1 : 0;
+}
+
+static void addVert ( Suite<Vector2d> & p, const Vector2d & v )
+{
+    if ( p.size() == 0 || p.las() != v ) p.inc() = v;
+}
+
+static nat advanceVert ( nat i, nat & cnt, nat n, bool inside, const Vector2d & v, Suite<Vector2d> & r )
+{
+    if ( inside ) addVert ( r, v );
+    ++cnt;
+    return i + 1 < n ? i + 1 : 0;
+}
+
+static bool insideConv ( const Vector2d & p, CCArrRef<Vector2d> & poly )
+{
+    const nat n = poly.size();
+    for ( nat i = 0; i < n; ++i )
+    {
+        const Vector2d & a = poly.cprev(i);
+        const Vector2d & b = poly[i];
+        if ( ( b - a ) % ( p - a ) < 0 ) return false;
+    }
+    return true;
+}
+
+static void copyPoly ( CCArrRef<Vector2d> & p, Suite<Vector2d> & r )
+{
+    r.resize ( p.size() );
+    for ( nat i = 0; i < p.size(); ++i ) r[i] = p[i];
+}
+
+static void finishPoly ( Suite<Vector2d> & tmp )
+{
+    if ( tmp.size() > 0 && tmp.las() == tmp[0] ) tmp.dec();
+    if ( tmp.size() < 3 ) tmp.resize();
+}
+
+void calcIntersect2c ( CCArrRef<Vector2d> & p1, CCArrRef<Vector2d> & p2, Suite<Vector2d> & res )
+{
+    res.resize();
+    const nat n = p1.size();
+    const nat m = p2.size();
+    if ( n < 3 || m < 3 ) return;
+    nat a = 0, b = 0, aa = 0, ba = 0;
+    int inflag = 0;
+    bool firstPoint = true;
+    Vector2d first;
+    const nat n2 = n + n;
+    const nat m2 = m + m;
+    while ( ( aa < n || ba < m ) && aa < n2 && ba < m2 )
+    {
+        const nat a1 = a > 0 ? a - 1 : n - 1;
+        const nat b1 = b > 0 ? b - 1 : m - 1;
+        const Vector2d A = p1[a] - p1[a1];
+        const Vector2d B = p2[b] - p2[b1];
+        if ( ! A )
+        {
+            a = advanceVert ( a, aa, n, false, p1[a], res );
+            continue;
+        }
+        if ( ! B )
+        {
+            b = advanceVert ( b, ba, m, false, p2[b], res );
+            continue;
+        }
+        const int cross = areaSign ( null2d, A, B );
+        const int aHB = areaSign ( p2[b1], p2[b], p1[a] );
+        const int bHA = areaSign ( p1[a1], p1[a], p2[b] );
+        const Def<Vector2d> ip = intersection ( Segment2d ( p1[a1], p1[a] ), Segment2d ( p2[b1], p2[b] ) );
+        int code = ip.isDef ? 1 : 0;
+        if ( ! code && cross == 0 && ( p2[b1] - p1[a1] ) % A == 0 )
+        {
+            const double t0 = A * p1[a1];
+            const double t1 = A * p1[a];
+            const double u0 = A * p2[b1];
+            const double u1 = A * p2[b];
+            if ( _min ( t0, t1 ) <= _max ( u0, u1 ) && _min ( u0, u1 ) <= _max ( t0, t1 ) )
+                code = 2;
+        }
+        if ( code == 1 )
+        {
+            if ( inflag == 0 && firstPoint )
+            {
+                aa = ba = 0;
+                firstPoint = false;
+                first = ip;
+            }
+            else if ( ! firstPoint && ip == first && res.size() > 0 )
+            {
+                break;
+            }
+            addVert ( res, ip );
+            if ( aHB > 0 ) inflag = 1;
+            else if ( bHA > 0 ) inflag = 2;
+        }
+        if ( code == 2 && A * B < 0 )
+        {
+            res.resize();
+            break;
+        }
+        if ( cross == 0 && aHB < 0 && bHA < 0 )
+        {
+            res.resize();
+            break;
+        }
+        if ( cross == 0 && aHB == 0 && bHA == 0 )
+        {
+            if ( inflag == 1 )
+                b = advanceVert ( b, ba, m, inflag == 2, p2[b], res );
+            else
+                a = advanceVert ( a, aa, n, inflag == 1, p1[a], res );
+        }
+        else if ( cross >= 0 )
+        {
+            if ( bHA > 0 )
+                a = advanceVert ( a, aa, n, inflag == 1, p1[a], res );
+            else
+                b = advanceVert ( b, ba, m, inflag == 2, p2[b], res );
+        }
+        else
+        {
+            if ( aHB > 0 )
+                b = advanceVert ( b, ba, m, inflag == 2, p2[b], res );
+            else
+                a = advanceVert ( a, aa, n, inflag == 1, p1[a], res );
+        }
+    }
+    if ( firstPoint )
+    {
+        if ( insideConv ( p1[0], p2 ) && insideConv ( p1.cnext(0), p2 ) )
+            copyPoly ( p1, res );
+        else if ( insideConv ( p2[0], p1 ) && insideConv ( p2.cnext(0), p1 ) )
+            copyPoly ( p2, res );
+    }
+    finishPoly ( res );
+}
 
 //**************************** 20.08.2026 *********************************//
 //
